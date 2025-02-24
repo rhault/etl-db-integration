c773973d54505433a88f3ff994762db526e4ed09
Rhault <raullloredagomez@gmail.com>
2025-02-23 23:07:12 -0300

    Extracting customer name and products (indexes and patterns

--- main.py
+++ main.py
@@ -6,37 +6,44 @@ number_of_pages = len(reader.pages)
 page = reader.pages[0]
 text = page.extract_text().split('\n')
 
-textRemove = {'Helio', 'Cesar', 'Nogueira', 'de', 'Almeida', 'FAT', 'COM.:', 'NOTA:', '-'}
+textRemove = {'SlpName', 'DocNum', 'pr_comissaovl_comissao', 'Helio', 'Cesar', 'Nogueira', 'de', 'Almeida', 'FAT', 'COM.:', 'NOTA:', '-', '20.000000%'}
 linesPDF = []
 
 for line in text:
   listLineText = line.split()
-  length = len(listLineText)
   listLineText = [_ for _ in listLineText if _ not in textRemove]
+  length = len(listLineText) - 1
   
-  itemproductExpr = r'^\d{2}\.\d{3}$'
-  dateExpr = r'^\d{2}-\d{4}$'
-  indexitemProduct = [index for index, _ in enumerate(listLineText) if re.match(itemproductExpr, _)]
-  indexDate = [index for index, _ in enumerate(listLineText) if re.match(itemproductExpr, _)]
-  print(indexitemProduct)
-  
-  endIndex = 0
+  patterns = {
+    'itemProduct': r'^\d{2}\.\d{3}$',
+    'date': r'^\d{1}-\d{4}$',
+    'docNum': r'^\d{6}$'
+  }
+
+  indexes = {}
+
+  for index, text in enumerate(listLineText):
+    for key, expr in patterns.items():
+      if key not in indexes and re.match(expr, text):
+        indexes[key] = {'index':index, 'value':text}
+
   joinClientName = []
   joinProductName = []
-  
-  for index, word in enumerate(listLineText):
-    #Client Name
-    if word.replace(' ', '').isalpha():
-      joinClientName.append(word)
-      endIndex += 1
-    else:
-      break
+  dateDoc = ''
 
-    #Product Name
-    
+  if indexes:
+    for index, text in enumerate(listLineText):
+      if index < indexes['docNum']['index']:
+        joinClientName.append(text)
+      elif index > indexes['itemProduct']['index'] and index < indexes['date']['index']:
+        joinProductName.append(text)
+      elif index == length:
+        dateDoc = text
   
-  clientName = ' '.join(joinClientName)
-  listLineText = listLineText[endIndex:length]
-  listLineText.insert(0, clientName)
-  #print(listLineText)
+    clientName = ' '.join(joinClientName)
+    productName = ' '.join(joinProductName)
+    listLineText = listLineText[indexes['date']['index']:indexes['date']['index'] + 12] #I need the elements that are 12 positions after the date
+    listLineText[0:1] = [dateDoc,clientName, indexes['itemProduct']['value'], productName, indexes['date']['value']]
+
+  print(listLineText)
 
