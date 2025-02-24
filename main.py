d30538a9bc5f79d4342295a40c779f83f0bc5e28
rhault <raullloredagomez@gmail.com>
2025-02-24 17:41:58 -0300

    functions and creator of csv

--- main.py
+++ main.py
@@ -1,49 +1,79 @@
 from PyPDF2 import PdfReader
 import re
+import csv
 
-reader = PdfReader("JANEIRO2025.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text().split('\n')
+def extractTextPdf(pdfPath):
+  reader = PdfReader(pdfPath)
+  extractedText = []
 
-textRemove = {'SlpName', 'DocNum', 'pr_comissaovl_comissao', 'Helio', 'Cesar', 'Nogueira', 'de', 'Almeida', 'FAT', 'COM.:', 'NOTA:', '-', '20.000000%'}
-linesPDF = []
+  for readerPage in reader.pages:
+    extractedText.append(readerPage.extract_text())
 
-for line in text:
-  listLineText = line.split()
-  listLineText = [_ for _ in listLineText if _ not in textRemove]
-  length = len(listLineText) - 1
-  
+  return extractedText
+
+def processText(textLines):
   patterns = {
     'itemProduct': r'^\d{2}\.\d{3}$',
     'date': r'^\d{1}-\d{4}$',
-    'docNum': r'^\d{6}$'
+    'docNum': r'^\d{5,6}$'
   }
 
-  indexes = {}
+  processedText = []
 
-  for index, text in enumerate(listLineText):
-    for key, expr in patterns.items():
-      if key not in indexes and re.match(expr, text):
-        indexes[key] = {'index':index, 'value':text}
+  for line in textLines.split('\n'):
+    listLineText = line.split()[5:]
+    length = len(listLineText) - 1
+    indexes = {}
 
-  joinClientName = []
-  joinProductName = []
-  dateDoc = ''
-
-  if indexes:
     for index, text in enumerate(listLineText):
-      if index < indexes['docNum']['index']:
-        joinClientName.append(text)
-      elif index > indexes['itemProduct']['index'] and index < indexes['date']['index']:
-        joinProductName.append(text)
-      elif index == length:
-        dateDoc = text
-  
-    clientName = ' '.join(joinClientName)
-    productName = ' '.join(joinProductName)
-    listLineText = listLineText[indexes['date']['index']:indexes['date']['index'] + 12] #I need the elements that are 12 positions after the date
-    listLineText[0:1] = [dateDoc,clientName, indexes['itemProduct']['value'], productName, indexes['date']['value']]
+      for key, expr in patterns.items():
+        if key not in indexes and re.match(expr, text):
+          indexes[key] = {'index':index, 'value':text}
 
-  print(listLineText)
+    if indexes:
+      joinClientName = []
+      joinProductName = []
+      dateDoc = ''
 
+      for index, text in enumerate(listLineText):
+        indexDate = indexes.get('date', {}).get('index', index)
+        indexItemProduct = indexes.get('itemProduct', {})
+
+        if index < indexes.get('docNum', {}).get('index', index):
+          joinClientName.append(text)
+        elif\
+          index > indexItemProduct.get('index', index) and \
+          index < indexDate:
+            joinProductName.append(text)
+        elif index == length:
+          dateDoc = text
+    
+      clientName = ' '.join(joinClientName)
+      productName = ' '.join(joinProductName)
+      listLineText = listLineText[indexDate:indexDate + 12] #I need the elements that are 12 positions after the date
+      listLineText[0:1] = [dateDoc,clientName, indexItemProduct.get('value', ''), productName]
+
+    processedText.append(listLineText)
+    
+  return processedText
+
+def main():
+  path ='JANEIRO2025.pdf'
+  extractedText = extractTextPdf(path)
+
+  with open('header.csv', 'r') as header:
+    fildnames = csv.DictReader(header).fieldnames
+    
+    with open('JANEIRO2025.csv', 'w', newline = '') as newFile:
+      csvWrite = csv.DictWriter(newFile, fieldnames = fildnames)
+      csvWrite.writeheader()
+
+      for text in extractedText:
+        processedData = processText(text)
+        for data in processedData:
+          if len(data) == len(fildnames):
+            for index, item in enumerate(fildnames):
+              rows = ''
+
+if __name__ == '__main__':
+  main()
